f1c2946c33a9268c5f060df52eb875977c1ac224
Anshul Kumar <89123591+theanshulkumar@users.noreply.github.com>
2022-11-01 11:31:30 +0530

    Time: 4 ms (21.32%), Space: 5.9 MB (68.68%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
+    // No Extra Memory
     string defangIPaddr(string address) {
+        int i=0;
+        while(i<address.length()) {
+            if(address.at(i)=='.') {
+                address.replace(i,1,"[.]");
+                i=i+3;
+            }
+            else
+                i++;
+        }
+        return address;
+    }
+    string TLE_defangIPaddr(string address) {
         string ans = "";
         for(int i=0;i<address.length();i++) {
             if(address.at(i)=='.') 
