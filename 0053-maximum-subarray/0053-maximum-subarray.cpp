e730771038bb065506f6a59f8af9a4c877d45d4d
Anshul Kumar <89123591+alwaysanshul@users.noreply.github.com>
2023-07-10 09:55:35 +0530

    Time: 172 ms (5.02%), Space: 67.7 MB (36.61%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
+    int maxSubArray(vector<int>& nums) { // TC,SC: nlogn,nlogn
+        int low = 0, high=nums.size()-1;
+        return maxSubArray_divide_conquer(nums, low, high);
+    }
+    int maxSubArray_n(vector<int>& nums) { // TC,SC: O(n),O(1)
         int max_sum=INT_MIN,current_sum=0;
         for(int i=0;i<nums.size();i++) {
             current_sum+=nums[i];
@@ -14,5 +18,26 @@ public:
             }
         }
         return max_sum;
+    }
+    int maxSubArray_divide_conquer(vector<int>& nums, int low, int high) {
+        if(low == high){
+            return nums[low];
+        }        
+        int mid = (low+high)>>1;
+        int left_max=INT_MIN,right_max=INT_MIN;
+        int temp=0;
+        for(int i=mid;i>=low;i--) {
+            temp+=nums[i];
+            left_max=max(left_max,temp);
+        }
+        temp=0;
+        for(int i=mid+1;i<=high;i++) {
+            temp+=nums[i];
+            right_max=max(right_max,temp);            
+        }
+        int tot_sub_max=left_max+right_max;
+        int max_left=maxSubArray_divide_conquer(nums,low,mid);
+        int max_right=maxSubArray_divide_conquer(nums,mid+1,high);
+        return max(tot_sub_max,max(max_left,max_right));
     }
 };
