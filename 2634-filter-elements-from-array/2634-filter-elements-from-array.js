73f55d4d3cb15c3df9cbcb13128f0e7d190c0016
Anshul Kumar <89123591+alwaysanshul@users.noreply.github.com>
2026-04-30 20:08:46 +0530

    Time: 39 ms (81.4%), Space: 53.1 MB (83.11%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    const n = arr.length
+    let filteredArr = []
+    for(let i=0;i<n;i++){
+        if(fn(arr[i],i))
+            filteredArr.push(arr[i])
+    }
+    return filteredArr
+};
