92f4c0a338117e12d2f381c84500393e21a2d9d0
Anshul Kumar <89123591+alwaysanshul@users.noreply.github.com>
2026-05-11 15:06:49 +0530

    Time: 31 ms (97.76%), Space: 53.6 MB (51.7%) - LeetHub

--- 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -3,38 +3,22 @@
  * @return {Object}
  */
 var expect = function(val) {
-    return {
-        toBe: function(n){
-            if(val===n)
-                return true
-            else
-                throw new Error("Not Equal")
-        },
-        notToBe: function(n){
-            if(val!==n)
-                return true
-            else
-                throw new Error("Equal")
-        }
-    };
+    let obj = {
+            toBe: function(n){
+                if(n!==null && val===n)
+                    return true
+                else 
+                    throw new Error("Not Equal")
+            },
+            notToBe: function(n){
+                if(val!==n)
+                    return true
+                else 
+                    throw new Error("Equal")
+            }
+    }
+    return obj
 };
-
-
-// Alternate Solution
-// const expect = (val) => {
-//     const throwError = (errorStr) => {throw new Error(errorStr)};
-    
-//     return {
-//         toBe:    (val2) => val2 === val || throwError('Not Equal'),
-//         notToBe: (val2) => val2 !== val || throwError('Equal'),
-//     };
-// };
-
-/**
- * expect(5).toBe(5); // true
- * expect(5).notToBe(5); // throws "Equal"
- */
-
 
 /**
  * expect(5).toBe(5); // true
