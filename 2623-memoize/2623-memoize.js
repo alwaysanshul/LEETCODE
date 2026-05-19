14606db9b1cef1c0744aaa2b8a4a9fb77370d465
Anshul Kumar <89123591+alwaysanshul@users.noreply.github.com>
2026-05-19 17:11:10 +0530

    Time: 237 ms (92.73%), Space: 97.3 MB (5.45%) - LeetHub

--- 2623-memoize/2623-memoize.js
+++ 2623-memoize/2623-memoize.js
@@ -2,29 +2,20 @@
  * @param {Function} fn
  * @return {Function}
  */
-
 function memoize(fn) {
     const cache = new Map()
-    let callCount = 0
+    return function(...args) {
+        const key = JSON.stringify(args)
 
-    const memoized = function(...args) {
-        let key = JSON.stringify(args)
         if(cache.has(key))
             return cache.get(key)
-        
-        callCount++
-        let res = fn(...args)
-        cache.set(key, res)
-        return res
-    }
 
-    memoized.callCount = function() {
-        return callCount
-    }
+        const result = fn(...args)
+        cache.set(key,result)
 
-    return memoized
+        return result
+    }
 }
-
 
 
 /** 
@@ -37,37 +28,3 @@ function memoize(fn) {
  * memoizedFn(2, 3) // 5
  * console.log(callCount) // 1 
  */
-
- function memoize_other_way(fn) {
-    let callCount = 0
-    cache = []
-    
-    const memoized = function(...args) {
-        for(let i=0;i<cache.length;i++) {
-            let entry = cache[i]
-            if( isSameArgs(entry.args, args)){
-                return entry.result
-            }
-        }
-        callCount++
-        let res = fn(...args)
-        cache.push({args:[...args],result:res})
-        return res        
-    }   
-
-    memoized.getCallCount = function() {
-        return callCount
-    }
-
-    return memoized
-}
-
-function isSameArgs(arg1, arg2){
-    if(arg1.length !== arg2.length)
-        return false
-    for( let i=0;i<arg1.length;i++){
-        if(arg1[i]!==arg2[i])
-            return false
-//     }
-//     return true
-// }
