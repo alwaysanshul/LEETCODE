a1e78077067106e4406b0b3dd716f3d70b744e25
Anshul Kumar <89123591+alwaysanshul@users.noreply.github.com>
2026-05-23 23:12:18 +0530

    Time: 49 ms (50.82%), Space: 53.3 MB (74.81%) - LeetHub

--- 2622-cache-with-time-limit/2622-cache-with-time-limit.js
+++ 2622-cache-with-time-limit/2622-cache-with-time-limit.js
@@ -1,48 +1,27 @@
-var TimeLimitedCache = function() {
-    this.cache = new Map()
+var TimeLimitedCache = function () {
+    this.cache = new Map();
 };
 
-/** 
- * @param {number} key
- * @param {number} value
- * @param {number} duration time until expiration in ms
- * @return {boolean} if un-expired key already existed
- */
-TimeLimitedCache.prototype.set = function(key, value, duration) {
-    let keyExists = this.cache.get(key)
-    if(keyExists){
-        clearTimeout(keyExists.timeId)
-    }
-    //Using arrow fn to keep 'this' referring to the Cache instance
-    let timeId = setTimeout( ()=>{
-        this.cache.delete(key)
-    }, duration) 
-    this.cache.set(key, {value, timeId})
-    return Boolean(keyExists)
+TimeLimitedCache.prototype.set = function (key, value, duration) {
+    const existingEntry = this.cache.get(key);
+    if (existingEntry) 
+        clearTimeout(existingEntry.timeoutId);
+        
+    const timeoutId = setTimeout(() => {
+        this.cache.delete(key);
+    }, duration);
+
+    this.cache.set(key, { value, timeoutId });
+
+    return !!existingEntry;
 };
 
-/** 
- * @param {number} key
- * @return {number} value associated with key
- */
-TimeLimitedCache.prototype.get = function(key) {
-    let keyExists = this.cache.has(key)
-    if(keyExists){
-        return this.cache.get(key).value
-    }
-    return -1
+TimeLimitedCache.prototype.get = function (key) {
+    return this.cache.has(key)
+        ? this.cache.get(key).value
+        : -1;
 };
 
-/** 
- * @return {number} count of non-expired keys
- */
-TimeLimitedCache.prototype.count = function() {
-    return this.cache.size
+TimeLimitedCache.prototype.count = function () {
+    return this.cache.size;
 };
-
-/**
- * const timeLimitedCache = new TimeLimitedCache()
- * timeLimitedCache.set(1, 42, 1000); // false
- * timeLimitedCache.get(1) // 42
- * timeLimitedCache.count() // 1
- */
