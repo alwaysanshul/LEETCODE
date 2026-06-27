42ad8aca16bc2a314d98cf800de63289ce6a5294
Anshul Kumar <89123591+alwaysanshul@users.noreply.github.com>
2026-06-28 00:02:48 +0530

    Time: 641 ms (19.06%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,32 +1,25 @@
-
-SELECT employee_id, department_id
-FROM Employee
-WHERE primary_flag='Y' OR 
-    employee_id in
-    (SELECT employee_id
-    FROM Employee
-    Group by employee_id
-    having count(employee_id)=1)
-
--- select
--- employee_id,
--- (
---     case 
---     when count(department_id) > 1 and primary_flag = 'Y' then department_id
---     when count(department_id) = 1 and primary_flag = 'N' then department_id
---     end
--- ) as department_id 
--- from
--- Employee
--- where 
--- (employee_id, department_id)
--- in
--- (
---     select
---     distinct employee_id, department_id
---     from
---     Employee 
---     where 
---     primary_flag = 'Y'
-
--- )
+select
+employee_id,
+department_id 
+from
+Employee
+where 
+(employee_id, department_id)
+in
+(
+    select
+    employee_id, department_id
+    from
+    Employee 
+    where 
+    primary_flag = 'Y'
+    UNION
+    select
+    employee_id, department_id
+    from
+    Employee 
+    group by
+    employee_id
+    having
+    COUNT(department_id) = 1
+)
